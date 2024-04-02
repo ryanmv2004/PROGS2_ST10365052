54e839e1b0793366597e88b4b175cb3edc6c33e0
ryanv <ryanv@LAPTOP-9BD8NE7F>
2024-04-02 14:23:11 +0200

    Add project files.

---
+++ CLDVapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CLDVapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
